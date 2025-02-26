f3b9f9692515225917ebda9992e55b424705e5fc
Thiago Rovina <ra124257@uem.br>
2025-02-25 23:22:29 -0300

    fazendo teste da API de buscar/Listar Fornecedor pelo Id

--- src/main/java/com/lastingsolutions/gerenciador/Gerenciador/controller/FornecedorController.java
+++ src/main/java/com/lastingsolutions/gerenciador/Gerenciador/controller/FornecedorController.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/fornecedor")
 public class FornecedorController {
@@ -18,5 +22,29 @@ public class FornecedorController {
     public ResponseEntity<Fornecedor> create(@RequestBody FornecedorRequestDTO body) {
         Fornecedor newFornecedor = fornecedorService.createFornecedor(body);
         return ResponseEntity.ok(newFornecedor);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Fornecedor>> getAllFornecedores() {
+        List<Fornecedor> fornecedores = fornecedorService.getAllFornecedores();
+        return ResponseEntity.ok(fornecedores);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Fornecedor> getFornecedorById(@PathVariable UUID id) {
+        Optional<Fornecedor> fornecedor = fornecedorService.getFornecedorById(id);
+        return fornecedor.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Fornecedor> updateFornecedor(@PathVariable UUID id, @RequestBody FornecedorRequestDTO body) {
+        Fornecedor updatedFornecedor = fornecedorService.updateFornecedor(id, body);
+        return ResponseEntity.ok(updatedFornecedor);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteFornecedor(@PathVariable UUID id) {
+        fornecedorService.deleteFornecedor(id);
+        return ResponseEntity.noContent().build();
     }
 }
