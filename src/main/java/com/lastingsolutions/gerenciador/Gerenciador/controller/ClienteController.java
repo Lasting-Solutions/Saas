e5ba7aa9cde7893e13d07ba241a67fbdc1f5ecaf
Thiago Rovina <ra124257@uem.br>
2025-02-25 23:00:45 -0300

    fazendo teste da API de buscar cliente pelo Id

--- src/main/java/com/lastingsolutions/gerenciador/Gerenciador/controller/ClienteController.java
+++ src/main/java/com/lastingsolutions/gerenciador/Gerenciador/controller/ClienteController.java
@@ -5,10 +5,11 @@ import com.lastingsolutions.gerenciador.Gerenciador.domain.cliente.ClienteReques
 import com.lastingsolutions.gerenciador.Gerenciador.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/cliente")
@@ -18,8 +19,32 @@ public class ClienteController {
     private ClienteService clienteService;
 
     @PostMapping
-    public ResponseEntity<Cliente> create(@RequestBody ClienteRequestDTO body){
-        Cliente newCliente = this.clienteService.createCliente(body);
+    public ResponseEntity<Cliente> create(@RequestBody ClienteRequestDTO body) {
+        Cliente newCliente = clienteService.createCliente(body);
         return ResponseEntity.ok(newCliente);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Cliente>> getAllClientes() {
+        List<Cliente> clientes = clienteService.getAllClientes();
+        return ResponseEntity.ok(clientes);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Cliente> getClienteById(@PathVariable UUID id) {
+        Optional<Cliente> cliente = clienteService.getClienteById(id);
+        return cliente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Cliente> updateCliente(@PathVariable UUID id, @RequestBody ClienteRequestDTO body) {
+        Cliente updatedCliente = clienteService.updateCliente(id, body);
+        return ResponseEntity.ok(updatedCliente);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCliente(@PathVariable UUID id) {
+        clienteService.deleteCliente(id);
+        return ResponseEntity.noContent().build();
     }
 }
